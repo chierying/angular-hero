ad57b2d210aebac0d2fd2e0878a9e69b1b84f98d
chierying <chierying@qq.com>
2019-11-18 15:31:21 +0800

    添加路由激活颜色

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,8 +10,6 @@ import {HeroFormComponent} from './hero-form/hero-form.component';
 
 // 配置路由
 const routes: Routes = [
-  // 添加这样一个完全匹配的"空路由"来匹配主页。并重定向到需要的路由
-  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
   {path: 'heroes', component: HerosComponent},
   // path 中的冒号（:）表示 :id 是一个占位符，它表示某个特定英雄的 id。
   {path: 'detail/:id', component: HeroDetailComponent},
@@ -20,6 +18,10 @@ const routes: Routes = [
   {path: 'directive', component: DirectiveComponent},
   {path: 'customPipe', component: CustomPipeComponent},
   {path: 'heroForm', component: HeroFormComponent},
+  // 添加这样一个完全匹配的"空路由"来匹配主页。并重定向到需要的路由
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  // 啥也没匹配到的时候
+  {path: '**', redirectTo: 'dashboard'},
 ];
 
 @NgModule({
